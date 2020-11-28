9838ef6c4ea1d50c63734c406c295c7644b799d8
Andy Peralta <aperalta686@gmail.com>
2020-11-27 20:20:12 -0500

    rename multiple files to better fit what the project is doing

--- musicPlayerV2/Library/LibraryVc.swift
+++ musicPlayerV2/Library/LibraryVc.swift
@@ -27,16 +27,20 @@ class LibraryVc: UIViewController, UITableViewDelegate, UITableViewDataSource {
 //        self.view.addSubview(child.view)
 //        child.view.frame = self.view.frame
 //        child.didMove(toParent: self)
+
       }
 
   let mediaItems = MPMediaQuery.songs().items!
+  let tableview = UITableView()
+  let cellID =  "albumCell"
+
 
   func numberOfSections(in tableView: UITableView) -> Int {
     return 1
   }
 
   func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-    return mediaItems.count
+    return 20
   }
 
   func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
