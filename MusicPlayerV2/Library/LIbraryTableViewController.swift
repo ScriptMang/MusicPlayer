31cb0f1a4306a64ccd89079a88ca3e031eb4fd0a
Andy Peralta <aperalta686@gmail.com>
2020-12-24 10:30:36 -0500

    Fixed the spacing in lbtv

--- MusicPlayerV2/Library/LIbraryTableViewController.swift
+++ MusicPlayerV2/Library/LIbraryTableViewController.swift
@@ -15,16 +15,6 @@ class LIbraryTableViewController: UITableViewController {
         super.viewDidLoad()
         setupTableView()
         addAlbumsIntoLst()
-        //        let child = AlbumTableViewController()
-        //        self.addChild(child)
-        //        self.view.addSubview(child.view)
-        //        child.view.frame = self.view.frame
-        //        child.didMove(toParent: self)
-        // Uncomment the following line to preserve selection between presentations
-        // self.clearsSelectionOnViewWillAppear = false
-
-        // Uncomment the following line to display an Edit button in the navigation bar for this view controller.
-        // self.navigationItem.rightBarButtonItem = self.editButtonItem
     }
 
     private func setupTableView() {
