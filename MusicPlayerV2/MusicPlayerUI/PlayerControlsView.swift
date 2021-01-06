f4af476eaf88a80d19c32975f7c3ba28bf8581c4
Andy Peralta <aperalta686@gmail.com>
2021-01-05 19:30:23 -0500

    Fixed constraints for the music player and renamed files to match the changes

--- MusicPlayerV2/MusicPlayerUI/PlayerControlsView.swift
+++ MusicPlayerV2/MusicPlayerUI/PlayerControlsView.swift
@@ -1,182 +1,116 @@
 //
-//  PlayerControlsView.swift
+//  PlayerUI.swift
 //  MusicPlayerV2
 //
-//  Created by Andy Peralta on 12/22/20.
+//  Created by Andy Peralta on 12/21/20.
 //
 
 import UIKit
 
-class PlayerControlsView: UIView {
+class PlayerUI: UIViewController {
+    //MARK: Buttons Initialization
 
-    private lazy var albumTitle: UILabel = {
-        let albumLabel = UILabel()
-        albumLabel.text = "AlbumTitle"
-        albumLabel.font = UIFont(name: "Hiragino Mincho ProN W3", size:  30.0)
-        albumLabel.sizeToFit()
-        return albumLabel
-    }()
-
-    private lazy var songTitle: UILabel = {
-        let songLabel = UILabel()
-        songLabel.text = "SongTitle"
-        songLabel.font = UIFont(name: "Hiragino Mincho ProN W3", size:  30.0)
-        return songLabel
-    }()
-
-    private lazy var artist: UILabel = {
-        let label = UILabel()
-        label.font = UIFont(name: "Hiragino Mincho ProN W3", size:  25.0)
-        label.text = "Artist"
-        return label
-    }()
-
-    private lazy var trackStackView: UIStackView = {
-        let stackview = UIStackView(arrangedSubviews: [songTitle, artist])
-        stackview.axis = .vertical
-        stackview.alignment = .leading
-        stackview.distribution = .equalSpacing
-        stackview.spacing = 20
-        return stackview
-    }()
-
-    private lazy var startTime: UILabel = {
-        let label = UILabel()
-        label.font = UIFont(name: "Hiragino Mincho ProN W3", size:  17.0)
-        label.text = "0:00 "
-        label.sizeToFit()
-        return label
-    }()
-
-    private lazy var endTime: UILabel = {
-        let label = UILabel()
-        label.font = UIFont(name: "Hiragino Mincho ProN W3", size:  17.0)
-        label.text = " 0:00"
-        label.sizeToFit()
-        return label
-    }()
-
-    private lazy var progressBar: UIProgressView = {
-        let pgBar = UIProgressView(progressViewStyle: .bar)
-        pgBar.progress = 0.10
-        pgBar.progressTintColor = .white
-        pgBar.backgroundColor = .gray
-        return pgBar
-    }()
-
-    private lazy var progressStackView: UIStackView = {
-        let stackView = UIStackView(arrangedSubviews: [startTime, progressBar, endTime])
-        stackView.axis = .horizontal
-        stackView.alignment = .center
-        stackView.distribution = .fill
-        stackView.sizeToFit()
-        return stackView
-    }()
-
-    private lazy var volumeButton: UIButton = {
+    private lazy var playButton: UIButton = {
         let button = UIButton(type: .system)
         button.tintColor = .white
-        let volumeButtonConfig = UIImage.SymbolConfiguration(pointSize: 32)
-        let volumeIcon = UIImage(systemName: "speaker.wave.2.circle", withConfiguration: volumeButtonConfig)
-        button.setImage(volumeIcon, for: .normal)
+
+        let playIconConfig = UIImage.SymbolConfiguration(pointSize: 40)
+        let playIcon = UIImage(systemName: "play.fill", withConfiguration: playIconConfig)
+        let pauseIcon = UIImage(systemName: "pause.fill",  withConfiguration: playIconConfig)
+
+        button.setImage(playIcon, for: .normal)
+        button.setImage(pauseIcon, for: .highlighted)
+        button.clipsToBounds = true
+        button.translatesAutoresizingMaskIntoConstraints = false
         return button
     }()
 
-    private lazy var volumePercentage: UILabel = {
-        let label = UILabel()
-        label.text = "100%"
-        label.textAlignment = .center
-        return label
+    private lazy var fastForwardButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        let fastForwardIconConfig = UIImage.SymbolConfiguration(pointSize: 20)
+        let fastForwardIcon = UIImage(systemName: "forward.fill", withConfiguration: fastForwardIconConfig)
+        button.setImage(fastForwardIcon, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
 
-    private lazy var volumeStack: UIStackView = {
-        let stackview = UIStackView(arrangedSubviews: [volumeButton, volumePercentage])
-        stackview.axis = .vertical
-        stackview.alignment = .fill
-        stackview.distribution = .equalSpacing
-        stackview.spacing = 15
-        return stackview
+    private lazy var rewindButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        let rewindButtonIconConfig = UIImage.SymbolConfiguration(pointSize: 20)
+        let rewindIcon = UIImage(systemName: "backward.fill", withConfiguration: rewindButtonIconConfig)
+        button.setImage(rewindIcon, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
 
-    private lazy var volumeSlider: UISlider = {
-        let slider = UISlider()
-        slider.maximumValue = 100.0
-        slider.minimumValue = 1.0
-        slider.isContinuous = true
-        slider.translatesAutoresizingMaskIntoConstraints = false
-        slider.backgroundColor = .black
-        slider.tintColor = .white
-        slider.addTarget(self, action: #selector(sliderValueChanged), for: .valueChanged)
 
-        UIView.animate(withDuration: 0.8) {
-            slider.setValue(80.0, animated: true)
-        }
-        return slider
+    private lazy var shuffleButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        let shuffleButtonConfig = UIImage.SymbolConfiguration(pointSize: 24)
+        let shuffleIcon = UIImage(systemName: "shuffle", withConfiguration: shuffleButtonConfig)
+        button.setImage(shuffleIcon, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
     }()
 
-    func delay(_ delay: Double, closure: @escaping () -> ()) {
-        let when = DispatchTime.now() + delay
-        DispatchQueue.main.asyncAfter(deadline: when, execute: closure)
+    private lazy var queueButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        let shuffleButtonConfig = UIImage.SymbolConfiguration(pointSize: 24)
+        let shuffleIcon = UIImage(systemName: "shuffle", withConfiguration: shuffleButtonConfig)
+        button.setImage(shuffleIcon, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
 
-    }
+    }()
 
-    @objc func sliderValueChanged(_ sender: UISlider!) {
-        let step: Float = 10.0
-        let approximateStepVal =  round(sender.value / step) * step
-        sender.value = approximateStepVal
+    private lazy var repeatButton: UIButton = {
+        let button = UIButton(type: .system)
+        button.tintColor = .white
+        let repeatButtonConfig = UIImage.SymbolConfiguration(pointSize: 24)
+        let repeatIcon = UIImage(systemName: "repeat", withConfiguration: repeatButtonConfig)
+        button.setImage(repeatIcon, for: .normal)
+        button.translatesAutoresizingMaskIntoConstraints = false
+        return button
+    }()
 
-    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
 
-    override init(frame: CGRect) {
-        super.init(frame: frame)
-        setupViews()
-    }
-    
-    required init?(coder: NSCoder) {
-        fatalError("init(coder:) has not been implemented")
-    }
-    
-    private func setupViews() {
-        // Add Views
+        // StackView for Play, Forward, Backward, Pause
+        let controlsStackView = UIStackView(arrangedSubviews: [rewindButton, playButton, fastForwardButton])
+        controlsStackView.axis = .horizontal
+        controlsStackView.alignment = .fill
+        controlsStackView.distribution = .fillEqually
+        controlsStackView.translatesAutoresizingMaskIntoConstraints = false
+        controlsStackView.spacing = 3
+        view.addSubview(controlsStackView)
 
-        for sub in [albumTitle, trackStackView, progressStackView, volumeStack, volumeSlider] {
-            addSubview(sub)
-            sub.translatesAutoresizingMaskIntoConstraints = false
-        }
+        //MARK: StackView for Song Options
+        let controlsStackView2 = UIStackView(arrangedSubviews: [repeatButton, shuffleButton, queueButton])
+        controlsStackView2.axis = .horizontal
+        controlsStackView2.alignment = .fill
+        controlsStackView2.distribution = .fillEqually
+        controlsStackView2.translatesAutoresizingMaskIntoConstraints = false
+        controlsStackView2.spacing = 0
+        view.addSubview(controlsStackView2)
 
-        let safeAreaLayoutGuide = self.safeAreaLayoutGuide
+        let marginLayoutGuide = view.layoutMarginsGuide
+        let controlsView = PlayerControlsView(frame: .zero)
+        view.addSubview(controlsView)
 
-        // Setup Constraints
-        NSLayoutConstraint.activate([
-            // Album Title
-            albumTitle.topAnchor.constraint(equalTo: topAnchor, constant:  80),
-            albumTitle.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant:  130),
+        NSLayoutConstraint.activate( [
+            //MusicPlayer Horizontal StackView
+            controlsStackView.topAnchor.constraint(equalTo: self.view.bottomAnchor, constant: 20),
+            controlsStackView.leadingAnchor.constraint(equalTo: marginLayoutGuide.leadingAnchor, constant:  50),
+            controlsStackView.bottomAnchor.constraint(lessThanOrEqualTo: view.bottomAnchor),
 
-            //TrackStackView
-            trackStackView.topAnchor.constraint(equalTo: topAnchor, constant: 550),
-            trackStackView.leadingAnchor.constraint(equalTo: safeAreaLayoutGuide.leadingAnchor, constant:  50),
-
-            //Volume StackView
-            volumeStack.topAnchor.constraint(equalTo:  trackStackView.topAnchor),
-            volumeStack.leadingAnchor.constraint(equalTo: trackStackView.trailingAnchor, constant:  120),
-            safeAreaLayoutGuide.trailingAnchor.constraint(lessThanOrEqualTo: volumeStack.trailingAnchor, constant:  10),
-            volumeStack.bottomAnchor.constraint(greaterThanOrEqualTo: trackStackView.bottomAnchor),
-
-            //ProgressStackView
-            progressStackView.topAnchor.constraint(equalTo: trackStackView.bottomAnchor, constant: 30),
-            progressStackView.leadingAnchor.constraint(equalTo: trackStackView.leadingAnchor),
-            trailingAnchor.constraint(lessThanOrEqualTo: progressStackView.trailingAnchor),
-
-            // Inside the PorgressStackView
-            progressBar.widthAnchor.constraint(equalToConstant: 250),
-            progressBar.heightAnchor.constraint(equalToConstant: 3),
-
-            //VolumeTouchBar
-            volumeSlider.topAnchor.constraint(equalTo: progressStackView.bottomAnchor, constant: 15),
-            safeAreaLayoutGuide.leadingAnchor.constraint(lessThanOrEqualTo: volumeSlider.leadingAnchor, constant: 50),
-            volumeSlider.widthAnchor.constraint(equalToConstant: 330),
-            volumeSlider.trailingAnchor.constraint(equalTo: trailingAnchor, constant:  380),
-            bottomAnchor.constraint(lessThanOrEqualTo: volumeSlider.bottomAnchor, constant: 5)
+            //MusicStack2
+            controlsStackView2.topAnchor.constraint(equalTo:  self.view.bottomAnchor, constant:  30),
+            controlsStackView2.leadingAnchor.constraint(equalTo: controlsStackView.trailingAnchor, constant: 58)
         ])
     }
 }
--- MusicPlayerV2/MusicPlayerUI/PlayerUI.swift
+++ MusicPlayerV2/MusicPlayerUI/PlayerUI.swift
@@ -1,133 +1,196 @@
 //
-//  PlayerUI.swift
+//  PlayerControlsView.swift
 //  MusicPlayerV2
 //
-//  Created by Andy Peralta on 12/21/20.
+//  Created by Andy Peralta on 12/22/20.
 //
 
 import UIKit
 
-class PlayerUI: UIViewController {
-    //MARK: Buttons Initialization
+class PlayerControlsView: UIView {
 
-    private lazy var playButton: UIButton = {
+    private lazy var albumTitle: UILabel = {
+        let albumLabel = UILabel()
+        albumLabel.text = "AlbumTitle"
+        albumLabel.font = UIFont(name: "Hiragino Mincho ProN W3", size:  30.0)
+        albumLabel.sizeToFit()
+        return albumLabel
+    }()
+
+    private lazy var albumImageView: UIImageView = {
+        let albumArtImage = UIImage(systemName: "music.note.house.fill")
+        let imageView = UIImageView(image: albumArtImage)
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.backgroundColor  = .white
+        return imageView
+    }()
+
+    private lazy var songTitle: UILabel = {
+        let songLabel = UILabel()
+        songLabel.text = "SongTitle"
+        songLabel.font = UIFont(name: "Hiragino Mincho ProN W3", size:  30.0)
+        return songLabel
+    }()
+
+    private lazy var artist: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Hiragino Mincho ProN W3", size:  25.0)
+        label.text = "Artist"
+        return label
+    }()
+
+    private lazy var trackStackView: UIStackView = {
+        let stackview = UIStackView(arrangedSubviews: [songTitle, artist])
+        stackview.axis = .vertical
+        stackview.alignment = .leading
+        stackview.distribution = .equalSpacing
+        stackview.spacing = 20
+        return stackview
+    }()
+
+    private lazy var startTime: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Hiragino Mincho ProN W3", size:  17.0)
+        label.text = "0:00 "
+        label.sizeToFit()
+        return label
+    }()
+
+    private lazy var endTime: UILabel = {
+        let label = UILabel()
+        label.font = UIFont(name: "Hiragino Mincho ProN W3", size:  17.0)
+        label.text = " 0:00"
+        label.sizeToFit()
+        return label
+    }()
+
+    private lazy var progressBar: UIProgressView = {
+        let pgBar = UIProgressView(progressViewStyle: .bar)
+        pgBar.progress = 0.10
+        pgBar.progressTintColor = .white
+        pgBar.backgroundColor = .gray
+        return pgBar
+    }()
+
+    private lazy var progressStackView: UIStackView = {
+        let stackView = UIStackView(arrangedSubviews: [startTime, progressBar, endTime])
+        stackView.axis = .horizontal
+        stackView.alignment = .center
+        stackView.distribution = .fill
+        stackView.sizeToFit()
+        return stackView
+    }()
+
+    private lazy var volumeButton: UIButton = {
         let button = UIButton(type: .system)
         button.tintColor = .white
-
-        let playIconConfig = UIImage.SymbolConfiguration(pointSize: 40)
-        let playIcon = UIImage(systemName: "play.fill", withConfiguration: playIconConfig)
-        let pauseIcon = UIImage(systemName: "pause.fill",  withConfiguration: playIconConfig)
-
-        button.setImage(playIcon, for: .normal)
-        button.setImage(pauseIcon, for: .highlighted)
-        button.clipsToBounds = true
-        button.translatesAutoresizingMaskIntoConstraints = false
+        let volumeButtonConfig = UIImage.SymbolConfiguration(pointSize: 32)
+        let volumeIcon = UIImage(systemName: "speaker.wave.2.circle", withConfiguration: volumeButtonConfig)
+        button.setImage(volumeIcon, for: .normal)
         return button
     }()
 
-    private lazy var fastForwardButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.tintColor = .white
-        let fastForwardIconConfig = UIImage.SymbolConfiguration(pointSize: 20)
-        let fastForwardIcon = UIImage(systemName: "forward.fill", withConfiguration: fastForwardIconConfig)
-        button.setImage(fastForwardIcon, for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
+    private lazy var volumePercentage: UILabel = {
+        let label = UILabel()
+        label.text = "100%"
+        label.textAlignment = .center
+        return label
     }()
 
-    private lazy var rewindButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.tintColor = .white
-        let rewindButtonIconConfig = UIImage.SymbolConfiguration(pointSize: 20)
-        let rewindIcon = UIImage(systemName: "backward.fill", withConfiguration: rewindButtonIconConfig)
-        button.setImage(rewindIcon, for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
+    private lazy var volumeStack: UIStackView = {
+        let stackview = UIStackView(arrangedSubviews: [volumeButton, volumePercentage])
+        stackview.axis = .vertical
+        stackview.alignment = .fill
+        stackview.distribution = .equalSpacing
+        stackview.spacing = 15
+        return stackview
     }()
 
+    private lazy var volumeSlider: UISlider = {
+        let slider = UISlider()
+        slider.maximumValue = 100.0
+        slider.minimumValue = 1.0
+        slider.isContinuous = true
+        slider.translatesAutoresizingMaskIntoConstraints = false
+        slider.backgroundColor = .black
+        slider.tintColor = .white
+        slider.addTarget(self, action: #selector(sliderValueChanged), for: .valueChanged)
 
-    private lazy var shuffleButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.tintColor = .white
-        let shuffleButtonConfig = UIImage.SymbolConfiguration(pointSize: 24)
-        let shuffleIcon = UIImage(systemName: "shuffle", withConfiguration: shuffleButtonConfig)
-        button.setImage(shuffleIcon, for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
+        UIView.animate(withDuration: 0.8) {
+            slider.setValue(80.0, animated: true)
+        }
+        return slider
     }()
 
-    private lazy var queueButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.tintColor = .white
-        let shuffleButtonConfig = UIImage.SymbolConfiguration(pointSize: 24)
-        let shuffleIcon = UIImage(systemName: "shuffle", withConfiguration: shuffleButtonConfig)
-        button.setImage(shuffleIcon, for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
+    func delay(_ delay: Double, closure: @escaping () -> ()) {
+        let when = DispatchTime.now() + delay
+        DispatchQueue.main.asyncAfter(deadline: when, execute: closure)
 
-    }()
+    }
 
-    private lazy var repeatButton: UIButton = {
-        let button = UIButton(type: .system)
-        button.tintColor = .white
-        let repeatButtonConfig = UIImage.SymbolConfiguration(pointSize: 24)
-        let repeatIcon = UIImage(systemName: "repeat", withConfiguration: repeatButtonConfig)
-        button.setImage(repeatIcon, for: .normal)
-        button.translatesAutoresizingMaskIntoConstraints = false
-        return button
-    }()
+    @objc func sliderValueChanged(_ sender: UISlider!) {
+        let step: Float = 10.0
+        let approximateStepVal =  round(sender.value / step) * step
+        sender.value = approximateStepVal
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
+    }
 
-        // AlbumArt
-        let musicHouseArt = UIImage(systemName: "music.note.house.fill")
-        let albumArt = UIImageView(image: musicHouseArt)
-        albumArt.translatesAutoresizingMaskIntoConstraints = false
-        albumArt.backgroundColor  = .white
-        view.addSubview(albumArt)
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    private func setupViews() {
+        // Add Views
 
-        // StackView for Play, Forward, Backward, Pause
-        let musicStackView = UIStackView(arrangedSubviews: [rewindButton, playButton, fastForwardButton])
-        musicStackView.axis = .horizontal
-        musicStackView.alignment = .fill
-        musicStackView.distribution = .fillEqually
-        musicStackView.translatesAutoresizingMaskIntoConstraints = false
-        musicStackView.spacing = 3
-        view.addSubview(musicStackView)
+        for sub in [albumTitle, albumImageView, trackStackView, progressStackView, volumeStack, volumeSlider] {
+            addSubview(sub)
+            sub.translatesAutoresizingMaskIntoConstraints = false
+        }
 
-        //MARK: StackView for Song Options
-        let musicStackView2 = UIStackView(arrangedSubviews: [repeatButton, shuffleButton, queueButton])
-        musicStackView2.axis = .horizontal
-        musicStackView2.alignment = .fill
-        musicStackView2.distribution = .fillEqually
-        musicStackView2.translatesAutoresizingMaskIntoConstraints = false
-        musicStackView2.spacing = 0
-        view.addSubview(musicStackView2)
+        // Setup Constraints
+        NSLayoutConstraint.activate([
+            // Album Title
+            albumTitle.topAnchor.constraint(equalTo: topAnchor),
+            albumTitle.centerXAnchor.constraint(equalTo: centerXAnchor),
 
-        let marginLayoutGuide = view.layoutMarginsGuide
-        let safeAreaLayoutGuide = view.safeAreaLayoutGuide
-
-        let controlsView = PlayerControlsView(frame: .zero)
-        view.addSubview(controlsView)
-
-        NSLayoutConstraint.activate( [
             // Album Art
-            albumArt.topAnchor.constraint(equalTo: view.topAnchor, constant: 120),
-            albumArt.leadingAnchor.constraint(equalTo: view.leadingAnchor, constant: 50),
-            safeAreaLayoutGuide.trailingAnchor.constraint(lessThanOrEqualTo: albumArt.trailingAnchor,  constant: 50),
-            safeAreaLayoutGuide.bottomAnchor.constraint(greaterThanOrEqualTo: albumArt.bottomAnchor,  constant: 50),
-            albumArt.heightAnchor.constraint(equalToConstant:  320),
-            albumArt.widthAnchor.constraint(equalToConstant:  320),
+            albumImageView.topAnchor.constraint(equalTo: topAnchor, constant: 120),
+            albumImageView.leadingAnchor.constraint(equalTo: leadingAnchor, constant: 50),
+            safeAreaLayoutGuide.trailingAnchor.constraint(lessThanOrEqualTo: albumImageView.trailingAnchor,  constant: 50),
+            safeAreaLayoutGuide.bottomAnchor.constraint(greaterThanOrEqualTo: albumImageView.bottomAnchor,  constant: 50),
+            albumImageView.heightAnchor.constraint(equalToConstant:  320),
+            albumImageView.widthAnchor.constraint(equalToConstant:  320),
 
-            //MusicPlayer Horizontal StackView
-            musicStackView.topAnchor.constraint(equalTo: albumArt.bottomAnchor, constant: 20),
-            musicStackView.leadingAnchor.constraint(equalTo: marginLayoutGuide.leadingAnchor, constant:  50),
-            musicStackView.bottomAnchor.constraint(lessThanOrEqualTo: view.bottomAnchor),
+            //TrackStackView
+            trackStackView.topAnchor.constraint(equalTo: albumImageView.bottomAnchor),
+            trackStackView.leadingAnchor.constraint(equalTo: leadingAnchor, constant:  50),
 
-            //MusicStack2
-            musicStackView2.topAnchor.constraint(equalTo:  albumArt.bottomAnchor, constant:  30),
-            musicStackView2.leadingAnchor.constraint(equalTo: musicStackView.trailingAnchor, constant: 58)
+            //Volume StackView
+            volumeStack.topAnchor.constraint(equalTo:  trackStackView.topAnchor),
+            volumeStack.leadingAnchor.constraint(equalTo: trackStackView.trailingAnchor, constant:  120),
+            volumeStack.trailingAnchor.constraint(lessThanOrEqualTo: trailingAnchor, constant: -10),
+            volumeStack.bottomAnchor.constraint(greaterThanOrEqualTo: trackStackView.bottomAnchor),
+
+            //ProgressStackView
+            progressStackView.topAnchor.constraint(equalTo: trackStackView.bottomAnchor, constant: 30),
+            progressStackView.leadingAnchor.constraint(equalTo: trackStackView.leadingAnchor),
+            progressStackView.trailingAnchor.constraint(lessThanOrEqualTo: trailingAnchor),
+
+            // Inside the PorgressStackView
+            progressBar.widthAnchor.constraint(equalToConstant: 250),
+            progressBar.heightAnchor.constraint(equalToConstant: 3),
+
+            //VolumeTouchBar
+            volumeSlider.topAnchor.constraint(equalTo: progressStackView.bottomAnchor, constant: 15),
+            volumeSlider.leadingAnchor.constraint(greaterThanOrEqualTo: leadingAnchor, constant: 50),
+            volumeSlider.widthAnchor.constraint(equalToConstant: 330),
+            volumeSlider.trailingAnchor.constraint(lessThanOrEqualTo: trailingAnchor),
+            volumeSlider.bottomAnchor.constraint(lessThanOrEqualTo: bottomAnchor, constant: -5)
         ])
     }
 }
